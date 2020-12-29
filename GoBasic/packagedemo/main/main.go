20cc6ce7f3a0ffbe1b7bc6c02e7b6314d909ce9c
dumin199101 <1766266374@qq.com>
2020-12-29 21:33:58 +0800

    包Package及File编译的区别测试

--- GoBasic/packagedemo/main/main.go
+++ GoBasic/packagedemo/main/main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	// 引入其他包的变量，默认从GoPath的src目录开始寻址，调用方式：包名.变量名
-	fmt.Print(utils.DbUtils)
+	fmt.Println(utils.DbUtils)
+	//build类型为Package,可以调用同一个包，但是不同文件下的函数及变量
+	//build类型为File，只能调用当前文件及引入文件下的函数及变量
+	res := add(1, 2)
+	fmt.Println(res)
 }
