83a56fd0ed0d824cdafc593fef79c099cecc675c
dumin199101 <1766266374@qq.com>
2020-12-30 16:41:31 +0800

    单元测试

---
+++ GoBasic/gotest/calc_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+)
+
+// 测试单文件 go test -v 单文件 依赖文件
+// 测试单个函数 go test -v -test.run 函数名
+func TestAdd(t *testing.T) {
+	res := add(1, 2)
+	if res == 3 {
+		t.Logf("测试通过，期望值：%d,实际值：%d", 3, res)
+	} else {
+		t.Fatalf("测试失败，期望值：%d,实际值：%d", 3, res)
+	}
+}
