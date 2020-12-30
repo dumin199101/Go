5342dc009c56202b8aa1e0882050ef0bcd1dc42a
dumin199101 <1766266374@qq.com>
2020-12-30 14:57:06 +0800

    日志操作

---
+++ GoBasic/chapter01/029.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	//日志操作
+
+	//设置日志格式
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+
+	//设置日志输出到文件
+	//log.SetOutput()
+
+	// 2020/12/30 14:45:21 Hello,日志开始记录
+	log.Println("Hello,日志开始记录")
+	//Fatal系列函数会在写入日志信息后调用os.Exit(1)。Panic系列函数会在写入日志信息后panic。
+	log.Panicln("对不起，出现错误了")
+	log.Fatalln("对不起，出现错误")
+
+}
