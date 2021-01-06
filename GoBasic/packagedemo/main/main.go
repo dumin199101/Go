434ae4ab8788f06c73b2b22602c51d8109f952e2
dumin199101 <1766266374@qq.com>
2021-01-06 12:24:48 +0800

    第三方包导入

--- GoBasic/packagedemo/main/main.go
+++ GoBasic/packagedemo/main/main.go
@@ -8,7 +8,8 @@ import (
 func main() {
 	// 引入其他包的变量，默认从GoPath的src目录开始寻址，调用方式：包名.变量名
 	fmt.Println(utils.DbUtils)
-	//build类型为Package,可以调用同一个包，但是不同文件下的函数及变量
+	utils.Person{}.SayHello()
+	//build类型为Package或者Directory,可以调用同一个包，但是不同文件下的函数及变量
 	//build类型为File，只能调用当前文件及引入文件下的函数及变量
 	res := add(1, 2)
 	fmt.Println(res)
--- GoBasic/packagedemo/utils/utils.go
+++ GoBasic/packagedemo/utils/utils.go
@@ -1,3 +1,14 @@
 package utils
 
+import "fmt"
+
 var DbUtils string = "DbUtils"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) SayHello() {
+	fmt.Println("Hello")
+}
